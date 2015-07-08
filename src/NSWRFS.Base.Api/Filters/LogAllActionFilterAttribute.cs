95cf275f4269cb3c15ff8e2ec7243d61295a5c04
James Darbyshire <james.darbyshire@rfs.nsw.gov.au>
2015-07-08 14:32:25 +1000

    Added async methods to Action filter

--- src/NSWRFS.Base.Api/Filters/LogAllActionFilterAttribute.cs
+++ src/NSWRFS.Base.Api/Filters/LogAllActionFilterAttribute.cs
@@ -1,17 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LogAllActionFilterAttribute.cs" company="NSW RFS">
+//   Copyright 2014 NSW Rural Fire Service, NSW Government, Australia
+// </copyright>
+// <summary>
+//   Defines the LogAllActionFilterAttribute type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace NSWRFS.Base.Api.Filters
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using System.Web.Http.Controllers;
     using System.Web.Http.Filters;
+
+    using Antlr.Runtime.Misc;
 
     using Microsoft.AspNet.Identity;
 
     using NLog;
 
+    /// <summary>
+    /// The log all action filter attribute.
+    /// </summary>
     public class LogAllActionFilterAttribute : ActionFilterAttribute
     {
         /// <summary>
@@ -19,6 +30,12 @@ namespace NSWRFS.Base.Api.Filters
         /// </summary>
         private static readonly Logger Nlog = LogManager.GetCurrentClassLogger();
 
+        /// <summary>
+        /// The on action executing.
+        /// </summary>
+        /// <param name="actionContext">
+        /// The action context.
+        /// </param>
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
             // Form the log string
@@ -31,6 +48,29 @@ namespace NSWRFS.Base.Api.Filters
             Nlog.Log(LogLevel.Trace, string.Format("request : {0}, username : {1}", requestString, username));
         }
 
+        /// <summary>
+        /// The on action executing async.
+        /// </summary>
+        /// <param name="actionContext">
+        /// The action context.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// The cancellation token.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public override Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            return Task.Run(() => this.OnActionExecuting(actionContext), cancellationToken);
+        }
+
+        /// <summary>
+        /// The on action executed.
+        /// </summary>
+        /// <param name="actionExecutedContext">
+        /// The action executed context.
+        /// </param>
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
             // Form the log string
@@ -41,6 +81,23 @@ namespace NSWRFS.Base.Api.Filters
 
             // Write a Log
             Nlog.Log(LogLevel.Trace, string.Format("request : {0}, username : {1}", responseString, username));
+        }
+
+        /// <summary>
+        /// The on action executed async.
+        /// </summary>
+        /// <param name="actionExecutedContext">
+        /// The action executed context.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// The cancellation token.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public override Task OnActionExecutedAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        {
+            return Task.Run(() => this.OnActionExecuted(actionExecutedContext), cancellationToken);
         }
     }
 }
